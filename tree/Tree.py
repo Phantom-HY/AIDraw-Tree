d8b606dfb59f33335a33a8dcf3832d7904cf7421
Phantom-HY <hyhh1209@hotmail.com>
2023-08-22 17:36:05 +0800

    add:NodeTree数据类型

--- tree/Tree.py
+++ tree/Tree.py
@@ -1,4 +1,4 @@
-class Tree:
+class NodeTree:
     def __init__(self, id, name):
         self.id = id
         self.name = name
@@ -15,3 +15,24 @@ class Tree:
 
     def get_name(self):
         return self.name
+
+
+class Tree:
+    def __init__(self, root, name):
+        self.root = root
+        self.node = [NodeTree(root, name)]
+
+    def add_node(self, id, name):
+        self.node.append(NodeTree(id, name))
+
+    def get_node(self, id):  # 获取指定节点信息
+        for node in self.node:
+            if node.get_id() == id:
+                return node
+        return None
+
+    def get_root(self):
+        return self.root
+
+    def get_node_list(self):
+        return self.node
